804b771d13df017a71f1ce9f82dfeae3f8e54bb8
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-01 15:58:02 +0530

    Variables and conditional statements

---
+++ JS/JS-INTRO/variables.js
@@ -0,0 +1,74 @@
+// console.log('Hello World')
+// console.log("Hello World")
+// console.log("Hello World");
+
+
+//Javascript is a Dynamically Typed Language,It can store any datatype while declaring.
+// let a;
+// a = 25;
+// console.log("The value of a is:", a);
+// console.log("The dataype of a :" , typeof a);
+
+// a = "Hello"
+// console.log("The value of a is:", a);
+// console.log("The dataype of a :" , typeof a);
+
+// a = false;
+// console.log("The value of a is:", a);
+// console.log("The dataype of a :" , typeof a);
+
+// single line comments
+
+/*
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+*/
+
+// let x = '10';
+// let y = '20';
+// let sum = x + y;
+// console.log(sum);
+
+// let a = 10;
+// let b = 3;
+
+// let result = a ** b;
+// console.log(result);
+
+// let result = a % b;
+// console.log(result);
+
+// let x = 123;
+// let y = '123';
+
+// Equal operator only checks the value and not the datatype.
+// console.log(x == y)
+// Strict equal operator checks both value and datatype.
+// console.log(x === y)
+
+// Decision making statements
+
+// let a = 10;
+// let b = 10;
+
+// if (a > b){
+//     console.log("a is greater than b");
+// }
+// else if (a == b)
+// {
+//     console.log("a is equal to b");
+// }
+// else{
+//     console.log("b is greater than a");
+// }
+
+
+
+
+
+
+
+
+
+
---
+++ practice/JS-INTRO/variables.js
@@ -0,0 +1,11 @@
+// console.log('Hello World')
+// console.log('Welcome to JavaScript');
+
+/*
+*/
+//
+
+//varaible declaration
+let a;
+
+console.log(a); 
