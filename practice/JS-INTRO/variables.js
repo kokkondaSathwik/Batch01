4b3540ec8f23ed6f8a52d88720db39d259a36007
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-03 17:11:46 +0530

    Array Operations for practice

--- practice/JS-INTRO/variables.js
+++ practice/JS-INTRO/variables.js
@@ -6,6 +6,101 @@
 //
 
 //varaible declaration
-let a;
+// let a;
 
-console.log(a); 
+// console.log(a); 
+
+// for (let x = 1 ; x <= 5; x++)
+// {
+//     console.log("For loop");
+// }
+
+// let x = 1;
+
+// while(x <= 5)
+// {
+//     console.log("while loop");
+//     x++;
+// }
+
+//functions
+
+// let choice = 1;
+
+// switch(choice){
+//     case 1:
+//         console.log("First Choice");
+//         break;
+    
+//     case 2:
+//         console.log("Second Choice");
+//         break;
+
+//     case 3:
+//         console.log("Third Choice");
+//         break;
+
+//     default:
+//         console.log("Invalid Choice and choice should be between 1 and 3");
+// }
+
+// functions
+
+// function findSumOfTwoNumbers(a,b){
+//     let sum = a + b;
+//     return sum;
+// }
+
+// let result = findSumOfTwoNumbers(5,10);
+// console.log(result);
+
+// let result2 = findSumOfTwoNumbers(20,30);
+// console.log(result2);
+
+// function expressions
+
+// let findSum = function(a,b){
+//     let sum = a + b;
+//     return sum;
+// }
+
+// let sum = findSum(5,10);
+// console.log(sum);
+
+
+//arrays
+
+let arr = [10,20,30]
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+// for(let index = 0;index <4; index++)
+// {
+//     console.log(arr[index]);
+// }
+
+// console.log(typeof arr);
+// console.log(arr);
+// let l1 = arr.unshift(1,2);
+// console.log(arr);
+// console.log(l1);
+
+// inserting the element at the end of the array
+// let l2 = arr.push(40,50);
+// console.log(arr);
+// console.log(l2);
+
+// arr.splice(2,0,123);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.splice(2,1);
+// console.log(arr);
