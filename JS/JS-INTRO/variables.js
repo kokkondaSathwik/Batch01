2c261824f731ae6401217d0b7da181710d768e72
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-05 06:55:25 +0530

    Objects

--- JS/JS-INTRO/variables.js
+++ JS/JS-INTRO/variables.js
@@ -187,37 +187,178 @@ This is a multi line comment
 //dynamic array is possible also in the compilation.
 // let students = [];
 
-let arr = [10,20,30,40];
-console.log(arr);
+// let arr = [10,20,30,40];
+// console.log(arr);
 
 // console.log(arr.length);
 
-let l1 = arr.unshift(1,2);
+// let l1 = arr.unshift(1,2);
 
-console.log(arr);
-console.log(l1);
+// console.log(arr);
+// console.log(l1);
 
 // insertion of elements at the end of the array
 
-let l2 = arr.push(100,200);
-console.log(arr);
-console.log(l2);
+    // let l2 = arr.push(100,200);
+    // console.log(arr);
+    // console.log(l2);
 
-arr.splice(2,0,123);
-console.log(arr);
+    // arr.splice(2,0,123);
+    // console.log(arr);
 
 
 // Removing the first element of the array
-arr.shift();
-console.log(arr);
+// arr.shift();
+// console.log(arr);
 
 //Removing the last element of the array
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 //Removing the element 
-arr.splice(1,1);
-console.log(arr);
+// arr.splice(1,1);
+// console.log(arr);
+
+
+
+
+// Objects
+
+// to create the student object with the sno,name,age and city as properties.
+
+// let student = {
+//     sno : 100, //it is property - 1 sno is key and 100 is the value of the key.
+//     name : 'Sathwik',
+//     age : 21,
+//     city : 'Hyderabad'
+// }
+
+// In Key we have to take the strings without quotes
+// But in the values we can take the strings with quotes. 
+
+
+// console.log(typeof student);
+
+
+// accessing the properties of the object
+
+// console.log(student.sno);
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.city);
+
+
+
+
+// Object Operations
+// Objects are unordered collection of data and it stores the key value pairs in the alphabetical order.
+
+//Accessing the properties of the object
+// console.log(emp.eno);
+// console.log(emp.name);
+// console.log(emp.city); //undefined
+
+//For Iteration over the properties of the object we have to use for in loop
+
+// for(let x in emp){
+//     console.log(emp[x]);
+// }
+
+// let emp = {
+//     eno:100,
+//     name : 'Sathwik'
+// };
+
+
+// Inserting the new properties dynamically
+
+// emp.city = 'Hyderabad'
+// console.log(emp);
+// emp.age = 21
+// console.log(emp);
+
+
+//Updating the existing properties of the object
+// emp.name = 'Sathwik Charry';
+
+
+
+// deleting the properties of the object
+// delete emp.eno;
+// console.log(emp);
+
+
+// complex object the object which contains the non primitives as its key value pairs is called as complex object.
+
+
+// let student = {
+//     sno : 100,
+//     name : 'Sathwik',
+//     marks : [98,65,68],
+//     address: {
+//         street : 'Bachupally X road',
+//         pincode : 506313,
+//         city : 'Hyderabad',
+//         state : 'Telangana'
+//     }
+// }
+
+// console.log(typeof student);
+
+// console.log(student);
+// console.log(student.sno);
+// console.log(student.name);
+// console.log(student.marks);
+// console.log(student.marks[0]);
+// console.log(student.marks[1]);
+// console.log(student.marks[2]);
+// console.log(student.address);
+// console.log(student.address.street);
+// console.log(student.address.pincode);
+// console.log(student.address.city);
+// console.log(student.address.state);
+
+// requirements
+// person object
+// properties : id,name,age
+// whats the best approach to create the 100 person objects with the above properties?
+
+// so the answer is the class
+
+
+// First i try to create the person object blueprint
+
+class Person{
+
+    constructor(id,name,age)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    // we are using this keyword to refer the current object which is being created by the class. eg person56 id this.id = id;
+}
+
+let person1 = new Person(1,'Sathwik',21);
+console.log(person1);
+
+let person2 = new Person(2,'Hema',20);
+console.log(person2);
+
+let person3 = new Person(3,'Poojitha',19);
+console.log(person3);
+
+
+
+// constructor is a special function but when the function which is using inside the class it becomes the method;
+// function outside the class or object is called as function only
+// but the same function when we are using inside the class or object it becomes the method.
+
+
+
+
+
+
 
 
 
