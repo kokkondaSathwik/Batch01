34202e45e2603f9df0a59f12981bfff5b4ccc14a
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-10 16:11:36 +0530

    Demo - async

---
+++ practice/JS-INTRO/async.js
@@ -0,0 +1,72 @@
+//synchronous JS 
+
+// console.log("Three customers reached the resturant");
+
+// console.log("Customer 1 ordered Biryani which takes 20 min to prepare");
+
+// // create a delay of 15 min 
+
+// for(let x=0;x<9000000000;x++)
+// {}
+
+// console.log("Customer 1 received the Biryani");
+
+// console.log("Customer 2 ordered Pizza which takes 15 min to prepare");
+
+// for(let x=0; x<6000000000;x++)
+// {}
+
+// console.log("Customer 2 received the Pizza");
+
+// console.log("Customer 3 ordered Soap which takes 2 min to prepare");
+
+// for(let x=0 ; x< 2000000000; x++)
+// {}
+
+// console.log("Customer 3 received the Soap");
+
+//Asynchronous JS
+//Timer Functions
+//Promise
+
+//Timer Functions
+
+
+// console.log("Start");
+// setTimeout(() => {
+//     console.log("callback executed");
+// }, 3000);
+
+// console.log("Hello");
+// console.log("Good Morning");
+
+// function test()
+// {
+//     console.log("This is a function");
+// }
+// test();
+
+// console.log("Three customers reached the resturant");
+
+// console.log("Customer 1 ordered Biryani which takes 20 min to prepare");
+
+// setTimeout(() => {
+//     console.log("Customer 1 received the Biryani");
+// }, 10000);
+
+// console.log("Customer 2 ordered Pizza which takes 15 min to prepare");
+
+// setTimeout(() => {
+//     console.log("Customer 2 received the Pizza");
+// }, 7000);
+
+// console.log("Customer 3 ordered Soap which takes 2 min to prepare");
+
+// setTimeout(() => {
+//     console.log("Customer 3 received the Soap");
+// }, 4000);
+
+
+// setInterval(() => {
+//     console.log("This is an interval function");
+// }, 3000);
