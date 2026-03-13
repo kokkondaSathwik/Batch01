e24448f1c50fbe59c1e2fa06b2845aa2af00b89e
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-13 23:46:45 +0530

    JSON

---
+++ practice/JS-INTRO/apis.js
@@ -0,0 +1,20 @@
+//Difference between the object and the JSON
+
+//Object
+
+// let obj={
+//     firstName : 'Sathwik',
+//     lastName : 'Reddy',
+//     age : 22
+// }
+
+// {"firstName": "Sathwik",
+//  "lastName": "Reddy",
+//  "age": 22
+// }
+
+
+// let jsonData = JSON.stringify(obj)
+
+// console.log(obj);
+// console.log(jsonData);
