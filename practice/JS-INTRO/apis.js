a39cf2461dbb5de38e7412217b2c33dbb39f39fb
Sathwik <sathwikkokkonda997@gmail.com>
2026-03-13 23:54:25 +0530

    Apis

--- practice/JS-INTRO/apis.js
+++ practice/JS-INTRO/apis.js
@@ -18,3 +18,14 @@
 
 // console.log(obj);
 // console.log(jsonData);
+
+
+// making API request ( HTTP request or AJAX request)
+// get request demonstration
+
+// get request using fetch API
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(response => response.json())
+// .then(json => console.log(json))
+// .catch(error => console.log(error))
